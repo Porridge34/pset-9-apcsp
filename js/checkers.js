9d139df8e49a3e7ea49e1e6bc68a646502cb485c
Ivy Ng <INg@ucvts.tec.nj.us>
2020-02-12 10:45:55 -0500

    checkers about 1/4 way done

---
+++ js/checkers.js
@@ -0,0 +1,70 @@
+///////////////////// CONSTANTS /////////////////////////////////////
+//tic tac toe
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+//tic tac toe
+let board;
+let turn;
+let win;
+let aIXorO;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+
+///////////////////// EVENT LISTENERS ///////////////////////////////
+//tic tac toe
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+///////////////////// FUNCTIONS /////////////////////////////////////
+//tic tac toe
+function getWinner() {
+  let winner = null;
+  let countBlack = 0;
+  let countWhite = 0;
+  board.forEach(function(mark){
+    (mark === "B")? countBlack++: countBlack;
+    (mark === "W")? countWhite++: countWhite;
+  });
+  (countBlack === 0)? winner = "White": winner = null;
+  (countWhite === 0)? winner = "Black": winner = null;
+  return winner;
+}
+function init() {
+  board = [
+    "", "B", "", "B", "", "B", "", "B",
+    "B", "", "B", "", "B", "", "B", "",
+    "", "B", "", "B", "", "B", "", "B",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "W", "", "W", "", "W", "", "W", "",
+    "", "W", "", "W", "", "W", "", "W",
+    "W", "", "W", "", "W", "", "W", ""
+  ];
+  turn = "Black";
+  win = null;
+  render();
+}
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+  message.textContent =
+    (win === "Black" || win === "White")? `${win} wins!` : `Turn: ${turn}`;
+}
+function takeTurn(e) {
+  if (!win) {
+    let selectedPiece = squares.findIndex(function(square){
+      return square === e.target;
+    })
+    (selectedPiece === "B" && turn === "Black") //where i left off last
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+    if (board[index] === "") {
+      (turn === "Black")? board[index] = "B": board[index] = "W";
+      turn = turn === "Black" ? "White" : "Black";
+      win = getWinner();
+      render();
+    }
+  }
+}
