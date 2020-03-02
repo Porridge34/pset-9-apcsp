ce319e58f8ead9cad72dc19254d859a43a8f81ad
porridge34 <ivy.yan.ng@gmail.com>
2020-03-01 19:51:13 -0500

    DSIOGJDSIO

--- js/connect4.js
+++ js/connect4.js
@@ -32,6 +32,9 @@ function getWinner() {
     }else if(mark.colour === board[index+8].colour && index%7<4 && index < 18){
       (mark.colour === board[index+16].colour && mark.colour === board[index+24].colour)?
       winner = mark.colour : c;
+    }else if(mark.colour === board[index+6].colour && index%7>3 && index ){
+      (mark.colour === board[index+12].colour && mark.colour === board[index+18].colour)?
+      winner = mark.colour : c;
     }
   });
   return winner;
